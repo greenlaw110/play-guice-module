377acbc8a8cdae69647475134ecdbfefbf0aee96
greenlaw110 <greenlaw110@gmail.com>
2010-09-09 09:41:03 +0800

    enable inject classes other than Controller/Mail/Job

---
+++ src/play/modules/guice/InjectSupport.java
@@ -0,0 +1,12 @@
+package play.modules.guice;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface InjectSupport {
+
+}
